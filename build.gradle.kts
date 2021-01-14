f4a6b20f686d9e8f67ab68539d2c91c78b7705c6
longdt12 <longdt57@gmail.com>
2021-01-14 14:11:37 +0700

    Add firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:${Versions.gradle}")
         classpath(kotlin(module = "gradle-plugin", version = "1.3.72"))
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.29.1-alpha")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.30.1-alpha")
+        classpath ("com.google.gms:google-services:4.3.4")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
