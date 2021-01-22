f64e0e0e576f6a7ea19e365d331c9cdc2d9852f4
longdt12 <longdt57@gmail.com>
2021-01-22 10:56:09 +0700

    Add tracking module

---
+++ tracking/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("lee.group.beat")
+}
+
+plugin_config {
+    ktlint = true
+}
+
+android {
+    compileSdkVersion(AppConfig.compileSdk)
+    buildToolsVersion(AppConfig.buildToolsVersion)
+
+    defaultConfig {
+        minSdkVersion(AppConfig.minSdk)
+        targetSdkVersion(AppConfig.targetSdk)
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    addKotlin()
+    addHilt()
+    addAppCompat()
+    addLifeCycle()
+    addTimber()
+
+    addFireBase()
+    implementation(AppDependencies.firebaseAnalytics)
+}
